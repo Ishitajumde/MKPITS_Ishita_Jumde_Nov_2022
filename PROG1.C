99e1b50158662ae76642d5883498a88d931f181d
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2022-11-24 22:34:42 +0530

    average value of item

---
+++ PROG1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int wt,num,res;
+	printf("enter the number");
+	scanf("%d%d",&wt,&num);
+	res=(wt+num)/2;
+	clrscr();
+	printf("avg=%d",res);
+	getch();
+}
