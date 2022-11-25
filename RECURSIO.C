670c8b12700a8220cb17592eac76d0491314933e
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2022-11-25 19:30:20 +0530

    recursion infinite solution

---
+++ RECURSIO.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void fun(int i)
+{
+	if(i==10)
+	return ;
+	printf("%d",i);
+	getch();
+	fun(i+1);
+	printf("\n %d",i);
+}
+void main()
+{
+	fun(1);
+	getch();
+}
+
