f4bc004b7c724921b06fd6648dc546fc050c48bf
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2022-11-27 19:43:24 +0530

    prime number using flag

---
+++ PRIMENUM.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num,i,f=0;
+  clrscr();
+  printf("enter any number");
+  scanf("%d",&num);
+  for(i=0;i<4;i++)
+  {
+    if(i==4)
+    {
+     printf("4 is present");
+    }
+    else
+    {
+      f=0;
+    }
+    if()
+  }
+getch();
+}
