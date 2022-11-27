f8065951b97f8e136bd638e63e65218871f07451
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2022-11-27 19:48:30 +0530

    string using userdefined func 2

---
+++ STRING2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>          //with retuntype with argument
+#include<conio.h>
+char stri(char name)
+{
+  return stri(name);
+}
+void main()
+{
+ char r,name;
+ printf("enter any name");
+  scanf("%s",&name);
+ r=stri(name);
+ printf("output=%s",r);
+  getch();
+}
