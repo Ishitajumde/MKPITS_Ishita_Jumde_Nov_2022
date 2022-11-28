013af6cb185638a81cdfa0d2cd0776e2572d17fe
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2022-11-28 23:29:28 +0530

    parameter of triangle

---
+++ TRIANGLE.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int an1,an2,an3,p,sum;
+clrscr();
+printf("enter the value for three angle");
+scanf("%d %d",&an1,an2,an3);
+sum=an1+an2+an3;
+  if(sum=180 && an1!=0 && an2!=0 && an3!=0)
+{
+      printf("traingle is valid");
+}
+if
+{
+   p=an1+an2+an3;
+}
+printf("parameter of triangl=%d",p);
+getch();
+}
+
+
