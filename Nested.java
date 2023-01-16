c1624640e1647a7b86d7c7ec5c6f986053becfa8
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2023-01-16 14:22:34 +0530

    nested if 1  statement in java

---
+++ Nested.java
@@ -0,0 +1,18 @@
+class Nested{
+public static void main(String[]args)
+{
+  int tm=70;
+  if(tm>50)
+  {
+    System.out.println("grade is B");
+  }
+     else if(tm<50)
+     {
+         System.out.println("grade is C");
+     }
+        else if(tm>80)
+             {
+	              System.out.println("grade is A");
+             }
+}
+}
