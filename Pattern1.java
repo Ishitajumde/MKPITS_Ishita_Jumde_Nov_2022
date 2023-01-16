2ce919bef75c28445ede9bc7bdf002fa1c786722
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2023-01-16 14:31:32 +0530

    pattern 1 in java

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+class Pattern1
+{
+public static void main(String[]args)
+{
+  int i,j;
+  for(i=0;i<4;i++)
+     {
+         for(j=65;j<=i+65;j++)
+            {
+                System.out.println(j );
+            }
+         System.out.println("\n");
+     }
+}
+}
