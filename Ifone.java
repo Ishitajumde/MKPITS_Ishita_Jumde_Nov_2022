968c247f2db39e3fba303772de0197433be306f5
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2023-01-16 14:21:26 +0530

    if  else statement in java

---
+++ Ifone.java
@@ -0,0 +1,14 @@
+class Ifone{
+public static void main(String[]args)
+{
+  int num=4;
+  if(num%2==0)
+  {
+    System.out.println("the num is even"+num);
+  }
+      else
+        {
+           System.out.println("the num is odd"+num);
+        }
+}
+}
