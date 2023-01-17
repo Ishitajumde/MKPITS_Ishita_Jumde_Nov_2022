5dfb8579ee71ae673f068cef06e6db960ec060c0
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2023-01-17 22:53:40 +0530

    arithmetic series in java

---
+++ aritpat.java
@@ -0,0 +1,20 @@
+class num
+{
+  int i;
+  void set()
+  {
+     for(i=1;i<30;i=i+3)
+       {
+         System.out.println(i);
+       }
+  }
+}
+
+class aritpat
+{
+  public static void main(String [] args)
+  {
+    num ob1= new num();
+    ob1.set();
+  }
+}
