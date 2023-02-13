7e4c3ec2aebdbe430c072f5d73440350a6cbca44
Ishitajumde <118798042+Ishitajumde@users.noreply.github.com>
2023-02-13 21:45:05 +0530

    prg_30 assg1_feb

---
+++ prg_30.java
@@ -0,0 +1,11 @@
+class prg_30{
+  public static void main(String[]args)
+  {
+    int i;
+    System.out.println("First 10 natural numbers");
+    for(i=1;i<=10;i++)
+    {
+       System.out.println(i);
+    }
+  }
+}
